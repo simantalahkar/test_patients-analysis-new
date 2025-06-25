dab43d78e01812f3e7ad42cd2d01bd4370df9b7f
simantalahkar <simantalahkar@hotmail.com>
2025-06-25 14:34:44 +0200

    initial commit

---
+++ calculate_mean.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+def calculate_mean(data):
+"""This function computes the mean of an array
+Input: One dimensional array
+Output: The mean of the array
+Usage:
+data=np.array([0,10,20,30])
+mean=calculate_mean(data)
+"""
+	mean=np.mean(data)
+	return mean 
+
